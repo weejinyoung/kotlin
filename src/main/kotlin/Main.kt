32570fd3fc76bf0775b4e66d3f1078bf16008d9b
weejinyoung <a8118199@gmail.com>
2023-03-30 23:44:00 +0900

    [ feat ] kotlin practice on Main 1
    fun val var enum

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,41 @@
+
 fun main(args: Array<String>) {
+
+    val answer = 42
+    val answer2: Int
+    //val answer3
     println("Hello World!")
     println(max(3, 4))
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    Color.RED.rgb()
+
 }
 
-fun max(a: Int, b: Int): Int {
-    return if (a > b) a else b
+fun max(a: Int, b: Int): Int = if (a > b) a else b
+
+fun messageSender(a: Int, b: Int): String {
+    val message: String = if(a > b) {
+        "Yes"
+    } else {
+        "No"
+    }
+    return message
 }
+
+fun messageSender2(): ArrayList<String> {
+    val message = arrayListOf("Java")
+    message.add("Kotlin")
+    return message
+}
+
+class Person(val name: String, val age: Int) {
+
+}
+
+enum class Color(val r: Int, val g: Int, val b: Int) {
+    RED(255, 0, 0), ORANGE(255, 165, 0);
+    fun rgb() = (r * 256 + g) * 256 + b
+}
+
+
+
